26bb3b27cdfedf96c5e12e3ac9a0a4110a47e598
nayyara_nivea@hotmail.com <gekesonmaycon@hotmail.com>
2021-06-03 20:46:01 +0200

    Deploy

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,36 +1,39 @@
 require('dotenv').config();
 
-module.exports = [
-    {
-        type: `postgres`,
-        url: process.env.DATABASE_URL,
-        synchronize: false,
-        logging: false,
-        entities: ["dist/src/entities/**{.ts,.js}"],
-        migrations: ["dist/src/database/migrations/**{.ts,.js}"],
-        cli: {
-            migrationsDir: 'dist/src/database/migrations'
+if (process.env.ENVIRONMENT === 'local') {
+    module.exports = [
+        {
+            type: 'postgres',
+            host: process.env.POSTGRES_HOST,
+            port: process.env.POSTGRES_PORT,
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            logging: true,
+            entities: ["./src/entities/**{.ts,.js}"],
+            migrations: ["./src/database/migrations/**{.ts,.js}"],
+            cli: {
+                migrationsDir: './src/database/migrations'
+            },
+            migrationsRun: false
+        }
+    ];
+} else {
+    module.exports = [
+        {
+            type: `postgres`,
+            url: process.env.DATABASE_URL,
+            synchronize: false,
+            logging: false,
+            entities: ["dist/src/entities/**{.ts,.js}"],
+            migrations: ["dist/src/database/migrations/**{.ts,.js}"],
+            cli: {
+                migrationsDir: 'dist/src/database/migrations'
+            },
+            ssl: {
+                rejectUnauthorized: false
+            },
+            migrationsRun: false
         },
-        ssl: {
-            rejectUnauthorized: false
-        },
-        migrationsRun: false
-    },
-    /* {
-        name: 'milibreBD',
-        type: 'postgres',
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        synchronize: true,
-        logging: true,
-        entities: ["dist/src/entities/**{.ts,.js}"],
-        migrations: ["dist/src/database/migrations/**{.ts,.js}"],
-        cli: {
-            migrationsDir: 'dist/src/database/migrations'
-        },
-    }, */
-];
+    ];
+}
 
